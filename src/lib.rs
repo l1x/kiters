37ae4692dbc0696450df8ba7972d75049938267c
Istvan <dev@l1x.be>
2026-01-13 00:53:28 +0100

    bd daemon sync: 2026-01-13 00:53:28

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,5 @@
 //! ## Modules
 //!
 //! - [`timestamp`]: Utilities for working with timestamps (specifically UTC formatted strings).
-//! - [`request_id`]: Fast request ID generator using sequential counter mapped to base64-like string.
 
-pub mod request_id;
 pub mod timestamp;
